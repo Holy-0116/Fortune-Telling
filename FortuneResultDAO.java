2ad2e7ef6066a919122b561d74b8a2d878e4682e
Holy-0116 <dragonguardian0116@gmail.com>
2021-03-12 15:30:56 +0900

    占い結果取得のロジック

---
+++ FortuneResultDAO.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FortuneResultDAO{
+
+    public String execute(int resultNum){
+        File file = new File("FortuneResult.txt");
+            List<String> fortuneResult = new ArrayList<String>();
+            try{
+                BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file),"UTF-8"));
+                
+                String line;
+                while((line = br.readLine()) != null){
+                    fortuneResult.add(line);
+                }
+               
+                int targetNum = resultNum % (fortuneResult.size());
+                String result = fortuneResult.get(targetNum);
+                return result;
+                
+            } catch (Exception e){
+                e.printStackTrace();
+                return "結果を取得できませんでした";
+            }
+            
+        }
+
+}
---
+++ GetFortuneResult.java
@@ -0,0 +1,21 @@
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+
+public class GetFortuneResult {
+    
+    private final List<String> SIGNS = Arrays.asList("やぎ座","みずがめ座","うお座","おひつじ座","おうし座","ふたご座","かに座","しし座","おとめ座","てんびん座","さそり座","いて座");
+
+    public int getResultNum(String sign){
+        LocalDate today = LocalDate.now();
+        int y = today.getYear();
+        int m = today.getMonthValue();
+        int d = today.getDayOfMonth();
+        
+        int indexOfSIGNS = SIGNS.indexOf(sign);
+        int resultNum = (y + m * d * (indexOfSIGNS + 1));
+        return resultNum;
+
+        
+    }
+}
